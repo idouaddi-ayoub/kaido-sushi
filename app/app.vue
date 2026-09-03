6af080758be634f09f6036ee4fc4a6dd5ee7cda6
PrOeZ <idouaddi.ayoub@gmail.com>
2026-09-03 22:34:57 +0100

    Added a hero section

--- app/app.vue
+++ app/app.vue
@@ -7,22 +7,19 @@ useHead({
   },
 });
 
-const title = "Nuxt Starter Template";
-const description =
-  "A production-ready starter template powered by Nuxt UI. Build beautiful, accessible, and performant applications in minutes, not hours.";
+const title = "Kaido Sushi";
+const description = "Some sushi stuff";
 
 useSeoMeta({
   title,
   description,
-  ogTitle: title,
-  ogDescription: description,
-  ogImage: "https://ui.nuxt.com/assets/templates/nuxt/starter-light.png",
-  twitterCard: "summary_large_image",
 });
 </script>
 
 <template>
   <UApp>
-    <NuxtPage />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </UApp>
 </template>
