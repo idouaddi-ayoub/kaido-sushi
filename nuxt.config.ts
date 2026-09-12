48d82877aedeebd385c7c8421aa3116abca44612
PrOeZ <idouaddi.ayoub@gmail.com>
2026-09-12 22:16:40 +0100

    Updated routeRules & site infos with robots settings & prerenderer

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+const siteUrl = process.env.NUXT_PUBLIC_SITE_URL || "http://localhost:3000";
+
 export default defineNuxtConfig({
   compatibilityDate: "2026-06-30",
   css: ["~/assets/css/main.css"],
@@ -10,14 +12,16 @@ export default defineNuxtConfig({
     "@tresjs/nuxt",
     "@vueuse/nuxt",
   ],
-  site: {
-    url: "https://kaidosushibar.fr",
-    name: "Kaido Sushi Bar",
+  site: { url: siteUrl, name: "Kaido Sushi Bar" },
+  linkChecker: {
+    failOnError: true,
   },
-
   icon: {
     serverBundle: {
       collections: ["lucide", "simple-icons", "circle-flags"],
+    },
+    clientBundle: {
+      scan: true,
     },
   },
   fonts: {
@@ -32,16 +36,26 @@ export default defineNuxtConfig({
   },
   devtools: {
     enabled: true,
-
     timeline: {
       enabled: true,
     },
   },
+  robots: {
+    allow: "/",
+    sitemap: ["/sitemap.xml"],
+  },
   routeRules: {
-    "/": { prerender: true },
-    "/en": { prerender: true },
+    "/": { prerender: false },
+    "/en": { prerender: false },
+  },
+  nitro: {
+    prerender: {
+      crawlLinks: false,
+      routes: ["/", "/en"],
+    },
   },
   i18n: {
+    baseUrl: siteUrl,
     locales: [
       { code: "fr", name: "French", language: "fr-FR" },
       { code: "en", name: "English", language: "en-US" },
