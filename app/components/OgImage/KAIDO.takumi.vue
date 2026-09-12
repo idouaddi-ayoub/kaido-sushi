f9a93febc85b7da53a1f67d9ef2a50f8ac503229
PrOeZ <idouaddi.ayoub@gmail.com>
2026-09-12 22:15:57 +0100

    OgImage (still needs work)

---
+++ app/components/OgImage/KAIDO.takumi.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const { title = "title" } = defineProps<{
+  title?: string;
+}>();
+</script>
+
+<template>
+  <div
+    class="h-full w-full flex items-start justify-start border-solid border-blue-500 border-12 bg-gray-50"
+  >
+    <div class="flex items-start justify-start h-full">
+      <div class="flex flex-col justify-between w-full h-full">
+        <h1 class="text-[80px] p-20 font-black text-left">
+          {{ title }}
+        </h1>
+        <p class="text-2xl pb-10 px-20 font-bold mb-0">bzi</p>
+      </div>
+    </div>
+  </div>
+</template>
