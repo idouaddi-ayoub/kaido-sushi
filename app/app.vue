c21a46b7a34322616d35cb98b448067ec2bf3f21
PrOeZ <idouaddi.ayoub@gmail.com>
2026-09-25 06:57:22 +0000

    Removed the defineOgImage function in index and added it to app

--- app/app.vue
+++ app/app.vue
@@ -1,24 +1,25 @@
 <script setup>
+const { t } = useI18n();
+
+useHead(useLocaleHead());
+
 useHead({
-  meta: [{ name: "viewport", content: "width=device-width, initial-scale=1" }],
   link: [{ rel: "icon", href: "/favicon.ico" }],
-  htmlAttrs: {
-    lang: "en",
-  },
 });
-const { t, locale } = useI18n();
-const localeHead = useLocaleHead();
-useHead(localeHead);
 
 useSeoMeta({
   title: () => t("home.seo.title"),
   description: () => t("home.seo.description"),
-  ogImage: "/KAIDO_SUSHI.webp",
   ogType: "website",
   ogSiteName: "Kaido Sushi Bar",
   ogTitle: () => t("home.seo.title"),
   ogDescription: () => t("home.seo.description"),
-  ogLocale: () => (locale.value === "fr" ? "fr_FR" : "en_US"),
+  twitterCard: "summary_large_image",
+});
+
+defineOgImage("KAIDO.takumi", {
+  title: t("home.seo.title"),
+  description: t("home.seo.description"),
 });
 </script>
 
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,10 +5,6 @@ import Hero from "../components/home/hero.vue";
 import Infos from "../components/home/infos.vue";
 import Menu from "../components/home/menu.vue";
 import Place from "../components/home/place.vue";
-
-defineOgImage("KAIDO.takumi", {
-  title: "Hello OG Image",
-});
 </script>
 
 <template>
